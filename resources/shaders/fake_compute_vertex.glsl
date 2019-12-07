2e9fdba35eb1776371c35042f09dd20c34240d33
0xBAMA <jon.baker1222@gmail.com>
2019-12-07 15:59:32 -0500

    working cellular automata

--- resources/shaders/fake_compute_vertex.glsl
+++ resources/shaders/fake_compute_vertex.glsl
@@ -21,36 +21,64 @@ void main()
 	//the location of voxel being considered
 	data[0] = ivec4(imageLoad(current,sample_location)*vec4(256));
 
+	//neighborhood for the wireworld simulation
+	int p = 1;
+	int n = -1;
+	int e_count = 0;
+
+
+
+
 
 
 
 
 
 	//we consider everything but the corners
-	if(current_data.r <10 && current_data.g <10 && current_data.b <10 && current_data.a > 200)
+	if(data[0].r <10 && data[0].g <10 && data[0].b <10 && data[0].a > 250)
 	{
-		//do I have electron neighbors? TBD
-		imageStore(next, sample_location, ivec4(255,0,0,255));	//goes to a red test color
+		data[ 1] = ivec4(imageLoad(current,sample_location+ivec3(p,0,0))*vec4(256));   // +x  0  0
+		data[ 2] = ivec4(imageLoad(current,sample_location+ivec3(n,0,0))*vec4(256));   // -x  0  0
+		data[ 3] = ivec4(imageLoad(current,sample_location+ivec3(0,p,0))*vec4(256));   // 0  +y  0
+		data[ 4] = ivec4(imageLoad(current,sample_location+ivec3(0,n,0))*vec4(256));   // 0  -y  0
+		data[ 5] = ivec4(imageLoad(current,sample_location+ivec3(0,0,p))*vec4(256));   // 0  0  +z
+		data[ 6] = ivec4(imageLoad(current,sample_location+ivec3(0,0,n))*vec4(256));   // 0  0  -z
+		data[ 7] = ivec4(imageLoad(current,sample_location+ivec3(p,p,0))*vec4(256));   // +x +y  0
+		data[ 8] = ivec4(imageLoad(current,sample_location+ivec3(p,n,0))*vec4(256));   // +x -y  0
+		data[ 9] = ivec4(imageLoad(current,sample_location+ivec3(n,p,0))*vec4(256));   // -x +y  0
+		data[10] = ivec4(imageLoad(current,sample_location+ivec3(n,n,0))*vec4(256));   // -x -y  0
+		data[11] = ivec4(imageLoad(current,sample_location+ivec3(p,0,p))*vec4(256));   // +x 0  +z
+		data[12] = ivec4(imageLoad(current,sample_location+ivec3(p,0,n))*vec4(256));   // +x 0  -z
+		data[13] = ivec4(imageLoad(current,sample_location+ivec3(n,0,p))*vec4(256));   // -x 0  +z
+		data[14] = ivec4(imageLoad(current,sample_location+ivec3(n,0,n))*vec4(256));   // -x 0  -z
+		data[15] = ivec4(imageLoad(current,sample_location+ivec3(0,p,p))*vec4(256));   // 0 +y  +z
+		data[16] = ivec4(imageLoad(current,sample_location+ivec3(0,p,n))*vec4(256));   // 0 +y  -z
+		data[17] = ivec4(imageLoad(current,sample_location+ivec3(0,n,p))*vec4(256));   // 0 -y  +z
+		data[18] = ivec4(imageLoad(current,sample_location+ivec3(0,n,n))*vec4(256));   // 0 -y  -z
+
+		//do I have 1 or 2 electron neighbors?
+		for(int i = 1; i < 19; i++)
+		{
+			if(data[i].r >250 && data[i].g >250 && data[i].a > 250)
+			{
+				e_count++;
+			}
+		}
+
+		if(e_count == 1 || e_count == 2)
+		{
+			imageStore(next, sample_location, ivec4(255,255,0,255));	//goes to electron head
+		}
+
+
 
 	}
-	else if(current_data.b >200 && current_data.g >200 && current_data.a > 200) //	cyan is electron tail
+	else if(data[0].b >250 && data[0].g >250 && data[0].a > 250) //	cyan is electron tail
 	{
 		imageStore(next,sample_location, ivec4(0,0,0,255));	//goes back to conductor
 	}
-	else if(current_data.r >200 && current_data.g >200 && current_data.a > 200)		//yellow is electron head
+	else if(data[0].r >250 && data[0].g >250 && data[0].a > 250)		//yellow is electron head
 	{
 		imageStore(next,sample_location, ivec4(0,255,255,255));	//goes to electron tail cyan
 	}
-
-
-
-
-
-
-
-
-	//LEAVE UNINVOLVED CELLS UNTOUCHED, DON'T REWRITE REDUNDANT DATA
-
-	//we handle data now as ivec4s, to make checking easier/more reliable
-
 }
