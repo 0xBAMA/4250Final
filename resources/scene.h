54c33e3b46f2a16f2a2375de1905ee19904b423f
0xBAMA <jon.baker1222@gmail.com>
2019-12-07 22:58:09 -0500

    ready to present

--- resources/scene.h
+++ resources/scene.h
@@ -94,7 +94,8 @@
 
 
 // #define TEX_PATH "resources/textures/models/wireworld0.png"             //automata
-#define TEX_PATH "resources/textures/models/wireworld1.png"             //automata
+// #define TEX_PATH "resources/textures/models/wireworld1.png"             //automata
+#define TEX_PATH "resources/textures/models/wireworld3.png"             //automata
 
 
 
